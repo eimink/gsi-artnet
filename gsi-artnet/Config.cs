d4c1399662305ba210148dbe251aaef61eabb182
Teppo Kauppinen <eimink@gmail.com>
2020-03-21 16:32:45 +0200

    Initial commit

---
+++ gsi-artnet/Config.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace gsi_artnet
+{
+    [Serializable]
+    class Config
+    {
+        public int DefaultScreen = 0;
+        public bool Debug = false;
+        public string ArtNetIP = "127.0.0.1";
+        public string ArtNetMask = "255.255.255.0";
+        public short ArtNetUniverse = 0;
+
+        public void Load()
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\config.json";
+            if (File.Exists(filePath))
+            {
+                TextReader reader = null;
+                try
+                {
+                    reader = new StreamReader(filePath);
+                    var fileContents = reader.ReadToEnd();
+                    var conf = JsonConvert.DeserializeObject<Config>(fileContents);
+                    Debug = conf.Debug;
+                    ArtNetIP = conf.ArtNetIP;
+                    ArtNetMask = conf.ArtNetMask;
+                }
+                catch (FileNotFoundException e)
+                {
+                    
+                }
+                finally
+                {
+                    if (reader != null)
+                        reader.Close();
+                }
+            }
+        }
+        public void Save()
+        {
+            TextWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\config.json", false);
+                writer.Write(JsonConvert.SerializeObject(this, Formatting.Indented));
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+    }
+}
---
+++ gsi-artnet/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSGSI;
+using CSGSI.Nodes;
+using System.Diagnostics;
+using ArtNet.Sockets;
+using ArtNet.Packets;
+using System.Net;
+
+namespace gsi_artnet
+{
+    class Program
+    {
+        public static Config config;
+        public static ArtNetSocket artNet;
+        public static GameStateListener gsl = new GameStateListener(3000);
+        public static byte[] dmxChannelData = new byte[9];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting gsi-artnet...");
+            if (config == null)
+            {
+                config = new Config();
+                config.Load();
+                Console.WriteLine("Config loaded.");
+            }
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+            gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+            InitializeArtNet();
+            if (!gsl.Start())
+            {
+                if (artNet != null)
+                {
+                    artNet.Close();
+                }
+                Console.WriteLine("Could not initialize ArtNet! Exiting...");
+                Environment.Exit(0);
+            }
+        }
+
+        static void InitializeArtNet()
+        {
+            Console.WriteLine("Initializing ArtNet.");
+            artNet = new ArtNetSocket();
+            artNet.EnableBroadcast = true;
+            Debug.WriteLine(artNet.BroadcastAddress);
+            artNet.Open(IPAddress.Parse(config.ArtNetIP), IPAddress.Parse(config.ArtNetMask));
+        }
+
+        static void OnProcessExit(object sender, EventArgs e)
+        {
+            config.Save();
+            if (artNet != null)
+            {
+                artNet.Close();
+            }
+            gsl.Stop();
+        }
+
+        static void OnNewGameState(GameState gs)
+        {
+            {
+                if (gs.Bomb.State == BombState.Planted) dmxChannelData[0] = 255;
+                else dmxChannelData[0] = 0;
+                if (gs.Bomb.State == BombState.Defusing) dmxChannelData[1] = 255;
+                else dmxChannelData[1] = 0;
+                if (gs.Bomb.State == BombState.Defused) dmxChannelData[2] = 255;
+                else dmxChannelData[2] = 0;
+                if (gs.Round.Bomb == BombState.Exploded) dmxChannelData[3] = 255;
+                else dmxChannelData[3] = 0;
+                if (gs.Round.Phase == RoundPhase.Over)
+                {
+                    if (gs.Round.WinTeam == RoundWinTeam.CT)
+                        dmxChannelData[4] = 255;
+                    else
+                        dmxChannelData[5] = 255;
+                }
+                else
+                {
+                    dmxChannelData[4] = 0;
+                    dmxChannelData[5] = 0;
+                }
+                if (gs.Map.Phase == MapPhase.Live) dmxChannelData[6] = 255;
+                else dmxChannelData[6] = 0;
+                if (gs.Map.Phase == MapPhase.Intermission) dmxChannelData[7] = 255;
+                else dmxChannelData[7] = 0;
+                if (gs.Map.Phase == MapPhase.GameOver) dmxChannelData[8] = 255;
+                else dmxChannelData[8] = 0;
+            }
+            SendPacketOverArtNet();
+        }
+
+        public static void SendPacketOverArtNet()
+        {
+            Console.WriteLine("Data: " + BitConverter.ToString(dmxChannelData));
+            if (artNet.IsBound)
+            {
+                ArtNetDmxPacket packet = new ArtNetDmxPacket();
+                packet.Universe = config.ArtNetUniverse;
+                packet.DmxData = new byte[dmxChannelData.Length];
+                Buffer.BlockCopy(dmxChannelData, 0, packet.DmxData, 0, dmxChannelData.Length);
+                artNet.Send(packet);
+            }
+        }
+    }
+}
